3ddd0e64092298f205ddd806946cfdd6aac2a186
uty117 <61403661+uty117@users.noreply.github.com>
2022-03-30 09:15:47 +0530

    Update dataFetch.js

--- src/hooks/dataFetch.js
+++ src/hooks/dataFetch.js
@@ -6,7 +6,7 @@ let anjali=addAnjali();
 let shriyam=addShriyam();
 let suraj=addSuraj();
 let utkarsh =addUtkarsh();
-let totalGames=3;
+let totalGames=5;
 export const getMostPicked = () =>  {
     return {
     labels: ["Shreyas", "Gaikwad", "Jadega", "Moeen", "Billings"],
